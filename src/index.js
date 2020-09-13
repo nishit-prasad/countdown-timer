f82330b265362e094d7ebd391974ee85397b0319
Nishit Prasad <prasad.nishit0@gmail.com>
2020-09-14 00:58:21 +0530

    STRY1: Added Label, Input, Date, TimerDisplay and reset & start button

--- src/index.js
+++ src/index.js
@@ -1,17 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+class Label extends React.Component {
+    render() {
+        return (
+            <label>
+                {this.props.label}
+            </label>
+        );
+    }
+}
+
+class Input extends React.Component {
+    render() {
+        return(
+            <div>
+                <input
+                    type={this.props.typeName}
+                />
+            </div>
+        );
+    }
+}
+
+class TimerDisplay extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>MM : DD : HH : MM : SS</h3>
+            </div>
+        );
+    }
+}
+
+class Button extends React.Component {
+    render() {
+        return (
+            <button type="button">{this.props.value}</button>
+        );
+    }
+}
+
+class TimerBoard extends React.Component {
+    renderButton(buttonName) {
+        return (
+            <Button
+                value={buttonName}
+            />
+        );
+    }
+    
+    renderLabel(label) {
+        return (
+            <Label label={label} />
+        );
+    }
+
+    renderInput(typeName) {
+        return (
+            <Input
+                typeName={typeName}
+            />
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    {this.renderLabel("Event Name")}
+                    {this.renderInput("input")}
+                    {this.renderInput("date")}
+                </div>
+                <div>
+                    <TimerDisplay />
+                </div>
+                <div>
+                    {this.renderButton("Reset")}
+                    {this.renderButton("Start")}
+                </div>
+            </div>
+            
+        );
+    }
+}
+
+// ==========================================
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <TimerBoard />,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
